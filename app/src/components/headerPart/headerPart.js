2c1234a8b6989f6c421e111a98ceb4af44f2b758
Yvonne Hayes <queyvonne@gmail.com>
2016-10-28 23:02:00 -0700

    messing around with hover and colors and stuff

--- app/src/components/headerPart/headerPart.js
+++ app/src/components/headerPart/headerPart.js
@@ -7,3 +7,21 @@ export default {
     this.styles = styles;
   }
 };
+
+$(document).ready(function(){
+  $('#workTab').mouseover(function(){
+    $('#workLine').css('background-color', 'pink');
+  });
+});
+
+// $(document).ready(function(){
+//   $('#workTab').mouseleave(function(){
+//     $('#tabs').css('background-color', 'lightsteelblue');
+//   });
+// });
+
+$(document).ready(function(){
+  $('#workTab').one('mouseover', function() {
+    $('#workTab').css('background-color', 'pink');
+  });
+});
