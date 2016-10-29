a45a727878331a3d46e3278d9e73aff38ac715de
Yvonne Hayes <queyvonne@gmail.com>
2016-10-29 12:18:51 -0700

    now changing tab colors with jquery. Getting closer

--- app/src/components/headerPart/headerPart.js
+++ app/src/components/headerPart/headerPart.js
@@ -5,23 +5,49 @@ export default {
   template,
   controller() {
     this.styles = styles;
+
+    function turnpink(){
+      $('#workTab').mouseover(function (){
+        $('#workTab').css('background-color', 'pink');
+      })
+    };
+    turnpink();
+
   }
 };
 
-$(document).ready(function(){
-  $('#workTab').mouseover(function(){
-    $('#workLine').css('background-color', 'pink');
-  });
-});
+
+
+
+
+
+
+
 
 // $(document).ready(function(){
-//   $('#workTab').mouseleave(function(){
-//     $('#tabs').css('background-color', 'lightsteelblue');
+//   $('#workTab').mouseover(function(){
+//     $('#workLine').css('background-color', 'pink');
 //   });
 // });
 
-$(document).ready(function(){
-  $('#workTab').one('mouseover', function() {
-    $('#workTab').css('background-color', 'pink');
-  });
-});
+
+
+// $(document).ready(function(){
+//   $('#workTab').one('mouseover', function() {
+//     $('#workTab').css('background-color', 'pink');
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $('#workTab').onclick(function(){
+//     $('#workTab').css('background-color', 'orange');
+//   });
+// });
+
+// var turnPink = function(){
+//   $('#workTab').mouseover(function() {
+//     $('#workTab').css({'background-color':'pink'});
+//   });
+// };
+//
+// turnPink();
